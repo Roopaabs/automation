aa4b3c291eae692495fac71f87c40da472395973
Roopa.BS@Dvara.com-at-382592957871 <roopa.bs@dvara.com>
2024-07-20 01:14:08 +0530

    first commit

--- src/main/java/pageObject/homePage.java
+++ src/main/java/pageObject/homePage.java
@@ -3,8 +3,13 @@ package pageObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 import org.junit.*;
-import org.junit.Assert;
+
 
 public class homePage extends Base {
 
@@ -19,9 +24,12 @@ public class homePage extends Base {
 
 	//method action
     public void verifyHomePageTitle() {
-        String expected = "Products";
-        String products = title.getText();
-        Assert.assertEquals(products, expected);
+    	  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    	   	 wait.until(ExpectedConditions.titleContains("Swag Labs"));
+    	        String expectedTitle = "Swag Labs";
+    	        String actualTitle = driver.getTitle();
+    	        System.out.println(actualTitle);        //Assert.assertEquals(actualTitle, expectedTitle, "Title does not match!");
+    	   
     }
 	
 	
